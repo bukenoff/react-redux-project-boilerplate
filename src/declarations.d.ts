2a4899d9f66ef81d393899b9751a693d85caadbc
serik <bukenoff.s@gmail.com>
2019-09-28 18:23:57 +0700

    renamed images to declarations, and declared scss type in it

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -5,3 +5,7 @@ declare module '*.jpeg';
 declare module '*.gif';
 declare module '*.bmp';
 declare module '*.tiff';
+declare module '*.scss' {
+  const content: {[className: string]: string};
+  export = content;
+}
