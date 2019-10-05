c91a0a602c7081a5000bfd924447c63d978a29bf
serik <bukenoff.s@gmail.com>
2019-10-05 11:07:54 +0700

    migrated to awesome-typescript-loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'awesome-typescript-loader',
+          loader: 'ts-loader',
         },
       },
       {
