939743a0aada3b1bdd19b77ec49f7355005c642e
serik <bukenoff.s@gmail.com>
2020-11-07 11:10:53 +0700

    edited tsconfig, added type definitions

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { connectRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 
 import thingsReducer from './things/reducers';
 // import moduleNameReducer from './module_folder_name/reducers';
@@ -11,10 +12,11 @@ const thingsPersistConfig = {
   storage,
 };
 
-const rootReducer = (history) => combineReducers({
-  things: persistReducer(thingsPersistConfig, thingsReducer),
-  // moduleName: moduleNameReducer,
-  router: connectRouter(history),
-});
+const rootReducer = (history: ReturnType<typeof createBrowserHistory>) =>
+  combineReducers({
+    things: persistReducer(thingsPersistConfig, thingsReducer),
+    // moduleName: moduleNameReducer,
+    router: connectRouter(history),
+  });
 
 export default rootReducer;
