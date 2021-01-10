0c15ec10e3f3b04fed106149e8fc364522ccc13e
serik <bukenoff.s@gmail.com>
2021-01-10 12:51:05 +0700

    removed duplicate rule declaration

--- webpack.config.js
+++ webpack.config.js
@@ -27,24 +27,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.[jt]sx?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-            options: {
-              plugins: [
-                isDevelopment && require.resolve('react-refresh/babel'),
-              ].filter(Boolean),
-            },
-          },
-        ],
-      },
-      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel'),
+            ].filter(Boolean),
+          },
         },
       },
       {
