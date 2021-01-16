d107d301dcfc4eb86f7a57c04a1d70c6da3081ba
serik <bukenoff.s@gmail.com>
2021-01-16 11:00:47 +0700

    removed babel, all of its dependencies and configurations, and hot refresh

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const outputDirectory = 'dist';
@@ -26,18 +25,6 @@ module.exports = {
   devtool: isDevelopment ? 'inline-source-map' : undefined,
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: [
-              isDevelopment && require.resolve('react-refresh/babel'),
-            ].filter(Boolean),
-          },
-        },
-      },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
@@ -129,6 +116,5 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
-    isDevelopment && new ReactRefreshWebpackPlugin(),
-  ].filter(Boolean),
+  ],
 };
