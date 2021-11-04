7e94bae1679d79dded09b23a099d49d237fd553f
serik <bukenoff.s@gmail.com>
2021-11-04 22:32:16 +0700

    added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,42 @@ import { ConnectedRouter } from 'connected-react-router';
 import { store, persistor, history } from './redux/store';
 import ExampleContainer from './containers/ExampleContainer';
 import { initSentry } from './services/sentry';
+import './i18n';
 
 import './styles/reset.scss';
 import './styles/global.scss';
+import { useTranslation } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  de: { nativeName: 'Deutsch' },
+};
 
 initSentry();
 
 const App: FC = () => {
+  const { i18n } = useTranslation();
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ConnectedRouter history={history}>
+          <div>
+            {Object.keys(lngs).map((lng) => (
+              <button
+                key={lng}
+                style={{
+                  fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal',
+                }}
+                type="submit"
+                onClick={() => {
+                  i18n.changeLanguage(lng);
+                }}
+              >
+                {lngs[lng as 'en' | 'de'].nativeName}
+              </button>
+            ))}
+          </div>
           <Route exact path="/" component={ExampleContainer} />
         </ConnectedRouter>
       </PersistGate>
--- src/containers/ExampleContainer/ExampleContainer.tsx
+++ src/containers/ExampleContainer/ExampleContainer.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 import { connect } from 'react-redux';
+import { useTranslation, Trans } from 'react-i18next';
+
 import { State } from '~/types';
 import * as styles from './styles.scss';
 import * as actions from '~/redux/things/things.actions';
@@ -18,9 +20,11 @@ type Props = ReturnType<typeof mapStateToProps> &
   typeof mapDispatchToProps & {};
 
 const ExampleContainer: FC<Props> = ({ things_list, addThing }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
-      <ExampleComponent text="hello" />
+      <ExampleComponent text={t('exampleContainer.wellcomeText')} />
     </div>
   );
 };
